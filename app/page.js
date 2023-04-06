90c86651d0603a0ae9a8201f2c881194ef539ab7
Denis <godlin086@gmail.com>
2023-04-06 18:31:34 +0300

    add search

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { createTheme, NextUIProvider, Card, Grid, Text, Row, Loading } from '@nextui-org/react'
+import { createTheme, NextUIProvider, Card, Grid, Text, Row, Input, Loading, Spacer, useInput } from '@nextui-org/react'
+import { SearchButton } from '@/components/SearchButton'
+import { SearchIcon } from '@/components/SearchIcon'
 //import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
 
 const theme = createTheme({
   type: 'dark',
@@ -17,65 +20,92 @@ const theme = createTheme({
 
 export default function Page() {
   const [products, setProducts] = useState([])
+  const { value, reset, bindings } = useInput('zara')
   
   useEffect(() => {
-    fetchData()
+    fetchData(value)
   }, [])
 
-  const fetchData = async () => {
-    const res = await fetch(`https://wbnew.vercel.app/api/hello`) //http://localhost:3000
+  const fetchData = async (query) => {
+    const res = await fetch(`https://wbnew.vercel.app/api/hello?q=${query}`)
+    //const res = await fetch(`http://localhost:3000/api/hello?q=${query}`)
     const data = await res.json()
     setProducts(data.data.products)
   }
 
   return (
-    <NextUIProvider theme={theme}>
-      {products.length === 0 && <Loading />}
-      <Grid.Container gap={2} justify="center">
-        {products.map((p, i) => {
-          const id = p.id.toString()
-          const vol = id.slice(0, -5)
-          let basket = '10'
-          if (vol < 1000)
-            basket = '05'
-          else if (vol < 1100)
-            basket = '06'
-          else if (vol < 1140)
-            basket = '07'
-          else if (vol < 1170)
-            basket = '08'
-          else if (vol < 1315)
-            basket = '09'
+    <div className={styles.container}>
+      <NextUIProvider theme={theme}>
+        <Row justify="center" align="center">
+          <Spacer y={5} />
+          <Input 
+            {...bindings}
+            rounded 
+            bordered 
+            size="lg" 
+            labelPlaceholder="Поиск"
+            initialValue="zara" 
+            color="primary" 
+            type="search"
+            contentRightStyling={false}
+            contentRight={
+              <SearchButton>
+                <SearchIcon />
+              </SearchButton>
+            }
+            onContentClick={() => {
+              setProducts([])
+              fetchData(value)
+            }}
+          />
+        </Row>
+        {products.length === 0 && <Row justify="center" align="center"><Loading /></Row>}
+        <Grid.Container gap={2} justify="center">
+          {products.map((p, i) => {
+            const id = p.id.toString()
+            const vol = id.slice(0, -5)
+            let basket = '10'
+            if (vol < 1000)
+              basket = '05'
+            else if (vol < 1100)
+              basket = '06'
+            else if (vol < 1140)
+              basket = '07'
+            else if (vol < 1170)
+              basket = '08'
+            else if (vol < 1315)
+              basket = '09'
 
-          return (
-            <Grid xs={6} sm={3} key={i}>
-              <Card isPressable onPress={() => window.open(`https://www.wildberries.ru/catalog/${id}/detail.aspx`, '_blank')}>
-                <Card.Body css={{ p: 0 }}>
-                  <Card.Image
-                    src={`https://basket-${basket}.wb.ru/vol${vol}/part${id.slice(0,-3)}/${id}/images/c516x688/1.jpg`}
-                    objectFit="cover"
-                    width="100%"
-                    height={400}
-                    alt={p.name}
-                  />
-                </Card.Body>
-                <Card.Footer css={{ justifyItems: "flex-start" }}>
-                  <Row wrap="wrap" justify="space-between" align="center">
-                    <Text b>{p.name + ' - ' + p.id}</Text>
-                    <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
-                      {new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB' }).format(p.salePriceU/100)}
-                    </Text>
-                  </Row>
-                </Card.Footer>
-                {/* css={{
-                    textGradient: "45deg, $blue600 -20%, $pink600 50%",
-                  }}*/}
-              </Card>
-            </Grid>
-          )
-        })}
-      </Grid.Container>
-    </NextUIProvider>
+            return (
+              <Grid xs={6} sm={3} key={i}>
+                <Card isPressable onPress={() => window.open(`https://www.wildberries.ru/catalog/${id}/detail.aspx`, '_blank')}>
+                  <Card.Body css={{ p: 0 }}>
+                    <Card.Image
+                      src={`https://basket-${basket}.wb.ru/vol${vol}/part${id.slice(0,-3)}/${id}/images/c516x688/1.jpg`}
+                      objectFit="cover"
+                      width="100%"
+                      height={300}
+                      alt={p.name}
+                    />
+                  </Card.Body>
+                  <Card.Footer css={{ justifyItems: "flex-start" }}>
+                    <Row wrap="wrap" justify="space-between" align="center">
+                      <Text b>{p.name + ' - ' + p.id}</Text>
+                      <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
+                        {new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB' }).format(p.salePriceU/100)}
+                      </Text>
+                    </Row>
+                  </Card.Footer>
+                  {/* css={{
+                      textGradient: "45deg, $blue600 -20%, $pink600 50%",
+                    }}*/}
+                </Card>
+              </Grid>
+            )
+          })}
+        </Grid.Container>
+      </NextUIProvider>
+    </div>
   )
 }
 
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,8 @@
 
 export default async function handler(req, res) {
-  const url = 'https://search.wb.ru/exactmatch/ru/male/v4/search?curr=rub&dest=12358291&fkind=1&page=1&query=zara&regions=80,64,38,4,115,83,33,68,70,69,30,86,40,1,66,48,110,22,31,114,111&resultset=catalog&sort=newly&spp=24&suppressSpellcheck=false'
+  const { q } = req.query
+
+  const url = 'https://search.wb.ru/exactmatch/ru/male/v4/search?curr=rub&dest=12358291&fkind=1&page=1&query=' + q + '&regions=80,64,38,4,115,83,33,68,70,69,30,86,40,1,66,48,110,22,31,114,111&resultset=catalog&sort=newly&spp=24&suppressSpellcheck=false'
   const r = await fetch(url)
   const data = await r.json()
 
