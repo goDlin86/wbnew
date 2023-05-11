ae9908f3926bdc143a801707fac2ade2f981b5b4
Denis <godlin086@gmail.com>
2023-05-11 07:17:01 +0300

    new api response

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,10 +1,12 @@
+import { NextResponse } from 'next/server'
 
-export default async function handler(req, res) {
-  const { q } = req.query
+export async function GET(request) {
+  const { searchParams } = new URL(request.url)
+  const { q } = searchParams.get('q')
 
   const url = 'https://search.wb.ru/exactmatch/ru/male/v4/search?curr=rub&dest=12358291&fkind=1&page=1&query=' + q + '&regions=80,64,38,4,115,83,33,68,70,69,30,86,40,1,66,48,110,22,31,114,111&resultset=catalog&sort=newly&spp=24&suppressSpellcheck=false'
   const r = await fetch(url)
   const data = await r.json()
 
-  res.status(200).json(data)
+  return NextResponse.json(data)
 }
