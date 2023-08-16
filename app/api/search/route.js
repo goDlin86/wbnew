6dcaf70609bbb19d79174ebbc90f9e43f0e0fa0c
Denis <godlin086@gmail.com>
2023-08-16 17:07:57 +0300

    catch error

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -6,7 +6,11 @@ export async function GET(request) {
 
   const url = 'https://search.wb.ru/exactmatch/ru/male/v4/search?curr=rub&dest=12358291&fkind=1&page=1&query=' + q + '&regions=80,64,38,4,115,83,33,68,70,69,30,86,40,1,66,48,110,22,31,114,111&resultset=catalog&sort=newly&spp=24&suppressSpellcheck=false'
   const r = await fetch(url)
-  const data = await r.json()
+  try {
+    const data = await r.json()
+  } catch (e) {
+    return console.error(e); // error in the above string (in this case, yes)!
+  }
 
   return NextResponse.json(data)
 }
