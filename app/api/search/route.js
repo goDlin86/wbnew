4e25bff915862b21422f09f1f57ea32e49775433
Denis <godlin086@gmail.com>
2023-08-23 17:28:29 +0300

    fixed query string

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -4,7 +4,7 @@ export async function GET(request) {
   const { searchParams } = new URL(request.url)
   const q = searchParams.get('q')
 
-  const url = 'https://search.wb.ru/exactmatch/ru/male/v4/search?curr=rub&dest=12358291&fkind=1&page=1&query=' + q + '&regions=80,64,38,4,115,83,33,68,70,69,30,86,40,1,66,48,110,22,31,114,111&resultset=catalog&sort=newly&spp=24&suppressSpellcheck=false'
+  const url = 'https://search.wb.ru/exactmatch/ru/male/v4/search?TestGroup=no_test&TestID=no_test&appType=1&curr=rub&dest=12358291&fkind=1&page=1&query=' + q + '&regions=80,38,83,4,64,33,68,70,30,40,86,69,1,31,66,22,110,48,114&resultset=catalog&sort=newly&spp=27&suppressSpellcheck=false&uclusters=8'
   const r = await fetch(url)
   try {
     const data = await r.json()
