5283bec9e3184bd667f0327e2593caeebbdd9f60
Denis <godlin086@gmail.com>
2023-09-14 18:06:07 +0300

    fix body

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -7,10 +7,11 @@ export async function GET(request) {
 
   //wb default
   let url = 'https://search.wb.ru/exactmatch/ru/male/v4/search?TestGroup=no_test&TestID=no_test&appType=1&curr=rub&dest=12358291&fkind=1&page=1&query=' + q.split('-')[1] + '&regions=80,38,83,4,64,33,68,70,30,40,86,69,1,31,66,22,110,48,114&resultset=catalog&sort=newly&spp=27&suppressSpellcheck=false&uclusters=8'
+  let body = {}
 
   if (market === 'brandly') {
     url = 'https://api.brandly.ru/api/ext/kt-api-extensions/catalog/product/loadCategoryProducts'
-    const body = {
+    body = {
       "size": 40,
       "query": {
         "pol": "muzhskoy"
