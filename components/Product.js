45fa2b3b8ab2792dbcfb5eb7d8b8888576ee5706
Denis <godlin086@gmail.com>
2024-05-22 19:32:54 +0300

    fixed price wb

--- components/Product.js
+++ components/Product.js
@@ -74,7 +74,7 @@ const WBProduct = ({ p }) => {
           // WebkitBackgroundClip: 'text',
           // WebkitTextFillColor: 'transparent' 
         }}>
-          {new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB' }).format(p.price.product/100)}
+          {new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB' }).format(p.sizes[0].price.product/100)}
         </div>
       </div>
     </a>
