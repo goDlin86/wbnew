3bc6793b74134509be1366c3072cb18d82486497
Denis <godlin086@gmail.com>
2024-05-22 18:38:06 +0300

    fixed wb url

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -7,7 +7,7 @@ export async function GET(request) {
   const brand = q.split('-')[1]
 
   //default wb market
-  let url = 'https://search.wb.ru/exactmatch/ru/male/v4/search?TestGroup=no_test&TestID=no_test&appType=1&curr=rub&dest=12358291&fkind=1&page=1&query=' + brand + '&regions=80,38,83,4,64,33,68,70,30,40,86,69,1,31,66,22,110,48,114&resultset=catalog&sort=newly&spp=27&suppressSpellcheck=false&uclusters=8'
+  let url = 'https://search.wb.ru/exactmatch/ru/male/v5/search?ab_testing=false&appType=1&curr=rub&dest=12358291&filters=xsubject&query=' + brand + '&resultset=filters&spp=30&suppressSpellcheck=false&uclusters=2'
   let body = {}
 
   if (market === 'brandly') {
